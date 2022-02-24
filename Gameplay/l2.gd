c2f94a6ed9982ca1d29a0c87eb26753843818f2d
Faris Ahmed T. Alotaibi <faa339@nyu.edu>
2022-02-23 23:10:27 -0500

    audio working, basic everything in place

--- Gameplay/l2.gd
+++ Gameplay/l2.gd
@@ -1,16 +1,18 @@
 extends Node2D
 onready var player = get_node("PlayerBody")
 
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	print(player.position.y)
-	if player.position.x > 960:
-		get_tree().change_scene("res://Gameplay/l2.tscn")
+	if player.position.x > 1440:
+		get_tree().change_scene("res://Gameplay/l3.tscn")
 	if player.position.y > 650:
-		get_tree().change_scene("res://Gameplay/l1.tscn")
+				get_tree().change_scene("res://Gameplay/l2.tscn")
---
+++ Gameplay/l3.gd
@@ -0,0 +1,31 @@
+extends Node2D
+onready var opencheck = false
+onready var plates = get_tree().get_nodes_in_group("plate")
+onready var door = get_node("TileMap/wall")
+onready var player = get_node("TileMap/PlayerBody")
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print(player.position.y)
+	if player.position.y > 1250:
+		get_tree().change_scene("res://Gameplay/l3.tscn")
+	
+	if opencheck:
+		return
+	
+	
+	for p in plates:
+		if p.counter < 3:
+			return
+	
+	door.open()
+	opencheck = true
